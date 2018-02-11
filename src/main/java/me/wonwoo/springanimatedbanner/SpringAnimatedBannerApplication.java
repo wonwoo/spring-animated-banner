ed13bec428ad62721f4fc218cd9e0b0aa3df3902
wonwoo <aoruqjfu@gmail.com>
2018-02-11 16:01:43 +0900

    Initial commit

---
+++ src/main/java/me/wonwoo/springanimatedbanner/SpringAnimatedBannerApplication.java
@@ -0,0 +1,12 @@
+package me.wonwoo.springanimatedbanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnimatedBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnimatedBannerApplication.class, args);
+	}
+}
---
+++ src/test/java/me/wonwoo/springanimatedbanner/SpringAnimatedBannerApplicationTests.java
@@ -0,0 +1,16 @@
+package me.wonwoo.springanimatedbanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAnimatedBannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
